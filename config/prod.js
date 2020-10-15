8252e95e9c4198d0779cd675fd75efde74cb0f86
Alien4Hire <jaymaynard84@gmail.com>
2020-10-15 16:38:32 -0400

    still broken again

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,8 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,//'629950976110-9gb17q5h00pk79blonjhvrfedpjjp57e.apps.googleusercontent.com',
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,//'blXNoGHzL1X7JT4H3hLNXc_q',
-    mongoURI: process.env.MONGO_URI,//'mongodb+srv://admin:Myfirst1@hubcap.v4hdg.mongodb.net/hubcap?retryWrites=true&w=majority',
+    mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY//'1Xxp382GgnXxYz49528',
 
 }
+//'mongodb+srv://admin:jvoeRs7qa7HQ8TNm@cluster0.jbqhq.mongodb.net/cluster?retryWrites=true&w=majority
