ca056d2f046eb6d4e788dd2cdec661e28feed502
Alien4Hire <jaymaynard84@gmail.com>
2020-10-21 11:16:14 -0400

    Added billing and Client side app!

--- config/keys.js
+++ config/keys.js
@@ -5,3 +5,9 @@ if (process.env.NODE_ENV === 'production') {
     //dev keys
     module.exports = require('./dev');
 }
+
+//publishable key
+//pk_test_51Bt52IGFN31Q4RRj6WMhLN8u7b39slIT5iyPWzh6xfAj1QmTOHnHPUuWoJgXyrT2weOcnoPyCXaSOCWlgbRpuecS00dy1Qenz7
+//secret key
+//sk_test_51Bt52IGFN31Q4RRjackM1SB2eZ2wBkXqgb6R4QTpHtxCYYwllruWoAS50cU2cu007KB9Nn3tgZDTHAIQbOW5JjZN00QxgcJgoY
+//Recurly to setup monthly payments
--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,9 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,//'629950976110-9gb17q5h00pk79blonjhvrfedpjjp57e.apps.googleusercontent.com',
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,//'blXNoGHzL1X7JT4H3hLNXc_q',
     mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY//'1Xxp382GgnXxYz49528',
+    cookieKey: process.env.COOKIE_KEY,//'1Xxp382GgnXxYz49528',
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY
 
-}
+};
 ///'mongodb+srv://admin:jvoeRs7qa7HQ8TNm@cluster0.jbqhq.mongodb.net/cluster?retryWrites=true&w=majority
