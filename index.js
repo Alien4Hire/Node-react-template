b54ecd77febf73440a4720a09553edaf6d2c1219
Alien4Hire <jaymaynard84@gmail.com>
2020-10-21 16:52:01 -0400

    Fixing prod database

--- index.js
+++ index.js
@@ -28,16 +28,16 @@ require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
-  //like our main.js file or main.css
+  // like our main.js file, or main.css file!
   app.use(express.static('client/build'));
 
-  //express will serve up index.html 
-  //if it doesnt recognize the route.
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
-};
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
