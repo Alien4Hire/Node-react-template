7d68e24d5ced4defce98906e254065f6118d543d
Alien4Hire <jaymaynard84@gmail.com>
2020-10-28 22:28:00 -0400

    fixed keys

--- config/prod.js
+++ config/prod.js
@@ -7,5 +7,11 @@ module.exports = {
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
     stripeSecretKey: process.env.STRIPE_SECRET_KEY,
     sendGridKey: process.env.SEND_GRID_KEY,
-    redirectDomain: process.env.redirectDomain
+    redirectDomain: process.env.redirectDomain,
+    FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID,
+    FACEBOOK_APP_SECRET: process.env.FACEBOOK_APP_SECRET,
+    clientToken: process.env.clientToken,
+    TWITTER_CONSUMER_KEY: process.env.TWITTER_CONSUMER_KEY,
+    TWITTER_CONSUMER_SECRET: process.env.TWITTER_CONSUMER_SECRET,
+    TWITTER_BEARER_TOKEN: process.env.TWITTER_BEARER_TOKEN
   };
